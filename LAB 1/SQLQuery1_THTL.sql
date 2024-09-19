5caf85754e9798667f8264c41e9b34234532482e
MinhisMinh <mainguyenphucminh175@gmail.com>
2024-09-19 15:31:17 +0700

    Change Lab1

---
+++ LAB 1/SQLQuery1_THTL.sql
@@ -0,0 +1,106 @@
+--- Mai Nguyen Phuc Minh - 23520930 - Lab 1 ---
+--- 1. Tạo các quan hệ và khai báo các khóa chính, khóa ngoại của quan hệ.
+--- TAO BANG ---
+CREATE DATABASE QUANLYBANHANG
+GO
+
+--- TAO CSDL QUANLY ---
+USE QUANLYBANHANG
+GO
+---TAO BANG KHACHHANG ---
+CREATE TABLE KHACHHANG
+(
+	MAKH CHAR(4) PRIMARY KEY,
+	HOTEN VARCHAR(40),
+	DCHI VARCHAR(50) NOT NULL,
+	SODT VARCHAR(20) NOT NULL,
+	NGSINH SMALLDATETIME,
+	NGDK SMALLDATETIME,
+	DOANHSO MONEY NOT NULL,
+);
+GO
+
+--- TAO BANG NHANVIEN ---
+CREATE TABLE NHANVIEN
+(
+	MANV CHAR(4) PRIMARY KEY,
+	HOTEN VARCHAR(40) NOT NULL,
+	SODT VARCHAR(20) NOT NULL UNIQUE,
+	NGVL SMALLDATETIME NOT NULL,
+)
+GO
+
+--- TAO BANG SANPHAM ---
+CREATE TABLE SANPHAM
+(
+	MASP CHAR(4) PRIMARY KEY,
+	TENSP VARCHAR(40) NOT NULL,
+	DVT VARCHAR(20),
+	NUOCSX VARCHAR(40),
+	GIA MONEY NOT NULL
+)
+GO
+
+--- TAO BANG HOADON ---
+CREATE TABLE HOADON
+(
+	SOHD INT PRIMARY KEY,
+	NGHD SMALLDATETIME,
+	MAKH CHAR(4) FOREIGN KEY REFERENCES KHACHHANG(MAKH),
+	MANV CHAR(4) FOREIGN KEY REFERENCES NHANVIEN(MANV),
+	TRIGIA MONEY
+)
+GO
+
+--- TAO BANG CTHD ---
+CREATE TABLE CTHD
+(
+	SOHD INT FOREIGN KEY REFERENCES HOADON(SOHD),
+	MASP CHAR(4) FOREIGN KEY REFERENCES SANPHAM(MASP),
+	SL INT,
+	PRIMARY KEY (SOHD, MASP)
+)
+GO
+
+--- 2. Thêm vào thuộc tính GHICHU có kiểu dữ liệu varchar(20) cho quan hệ SANPHAM.
+ALTER TABLE SANPHAM
+ADD GHICHU VARCHAR(20)
+GO
+
+--- 3. Thêm vào thuộc tính LOAIKH có kiểu dữ liệu là tinyint cho quan hệ KHACHHANG.
+ALTER TABLE KHACHHANG
+ADD LOAIKH TINYINT
+GO
+
+--- 4. Sửa kiểu dữ liệu của thuộc tính GHICHU trong quan hệ SANPHAM thành varchar(100).
+ALTER TABLE SANPHAM 
+ALTER COLUMN GHICHU VARCHAR(100)
+GO
+
+--- 5. Xóa thuộc tính GHICHU trong quan hệ SANPHAM.
+ALTER TABLE SANPHAM
+DROP COLUMN GHICHU
+GO
+
+--- 6. Làm thế nào để thuộc tính LOAIKH trong quan hệ KHACHHANG có thể lưu các giá trị là: “Vang lai”, “Thuong xuyen”, “Vip”, …
+ALTER TABLE KHACHHANG
+ALTER COLUMN LOAIKH VARCHAR(20)
+GO
+
+--- 7. Đơn vị tính của sản phẩm chỉ có thể là (“cay”,”hop”,”cai”,”quyen”,”chuc”)
+ALTER TABLE SANPHAM
+ADD CONSTRAINT CK_DVT CHECK(DVT in ('cay','hop','cai','quyen','chuc'))
+GO
+
+--- 8. Giá bán của sản phẩm từ 500 đồng trở lên
+ALTER TABLE SANPHAM
+ADD CONSTRAINT CK_GIA CHECK(GIA >= 500)
+GO
+
+--- 9. Mỗi lần mua hàng, khách hàng phải mua ít nhất 1 sản phẩm.
+ALTER TABLE CTHD ADD CONSTRAINT CK_SL CHECK(SL >= 1)
+GO
+
+--- 10.	Ngày khách hàng đăng ký là khách hàng thành viên phải lớn hơn ngày sinh của người đó.
+ALTER TABLE KHACHHANG ADD CONSTRAINT CK_NGDK CHECK(NGDK > NGSINH)
+GO
